4d5c3db45d020a1221c7b001fe501f3e1708314d
jeiosorio <jeison.osorio48375@udecaldas.edu.co>
2023-10-12 00:23:13 -0500

    add ejemplo herencia estudy

---
+++ src/edu/caldas/ingenieria/POO/estudiante.java
@@ -0,0 +1,26 @@
+package ucaldas.edo.ejemplo.herencia;
+
+import ucaldas.edo.ejemplo.herencia.persona;
+
+public class estudiante {
+	
+	public class Estudiante extends persona{
+	
+		private long codigo = 0;
+		public long getCodigo() {
+			return codigo;
+		}
+		public void setCodigo(long codigo) {
+			this.codigo = codigo;
+		}
+		public Estudiante() {
+			
+		}
+		public void aprobar() {
+			
+		}
+		public void reprobar() {
+			
+		}
+	}
+}
