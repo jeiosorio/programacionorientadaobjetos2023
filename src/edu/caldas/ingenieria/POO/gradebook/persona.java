112aa4df9e9751cd05b0cfafdd277b9712059cfd
jeiosorio <jeison.osorio48375@udecaldas.edu.co>
2023-10-12 00:14:13 -0500

    add ejemplo herencia person

---
+++ src/edu/caldas/ingenieria/POO/gradebook/persona.java
@@ -0,0 +1,32 @@
+package ucaldas.edo.ejemplo.herencia;
+
+public class persona {
+	
+	private String name = "";
+	private String fechadenacimiento = "";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFechadenacimiento() {
+		return fechadenacimiento;
+	}
+	public void setFechadenacimiento(String fechadenacimiento) {
+		this.fechadenacimiento = fechadenacimiento;
+	}
+	public persona() {
+		
+	}
+	public void comer () {
+	}
+	public void dormir() {
+	
+	}
+	public void respirar() {
+		
+	}
+	
+}
